0439a50b984c79cb6f3414e87315ff38d14c68a5
Regis Lima <regislima1391@gmail.com>
2019-04-05 13:03:23 -0300

    Traducoes e ajustes no layout

--- ToDoList/Models/ToDo.cs
+++ ToDoList/Models/ToDo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,14 +10,19 @@ namespace ToDoList.Models
     {
         public int Id { get; set; }
 
+        [Display(Name ="Nome")]
         public string Name { get; set; }
 
+        [Display(Name = "Descrição")]
         public string Description { get; set; }
 
+        [Display(Name = "Início")]
         public DateTime Initial { get; set; }
 
+        [Display(Name = "Término")]
         public DateTime Final { get; set; }
 
+        [Display(Name = "Prioridade")]
         public string Priority { get; set; }
     }
 }
